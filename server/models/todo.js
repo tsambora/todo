4ea5b0e69ff4510e5d10b27a322ebe4e167a66bc
Tino Sambora <krisnasambora@gmail.com>
2019-02-27 11:45:50 +0800

    kube pipeline test

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Todo) {
+  Todo.greet = function(msg, cb) {
+    cb(null, 'Greetings... ' + msg);
+  };
 
+  Todo.remoteMethod('greet', {
+    accepts: {arg: 'msg', type: 'string'},
+    returns: {arg: 'greeting', type: 'string'},
+  });
 };
